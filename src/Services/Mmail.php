d627205491380cb26a3d2b95b492ce1a8ad60b06
Leonardo Lemos <leonardo@mixd.com.br>
2017-02-23 23:51:11 -0300

    elimina redundancia da Closure que efetua o disparo de email

--- src/Services/Mmail.php
+++ src/Services/Mmail.php
@@ -8,71 +8,28 @@ use Mail;
 
 class Mmail
 {
-    public function __construct()
-    {
-
-    }
-
     public function send($data, $slug = null, $from = [], $template = 'emails.default')
     {
-        $db = $this->findSlug($slug);
-
         $data = (is_array($data)) ? $data : ['content' => $data];
-        return Mail::send($template, $data, function ($message) use ($db, $from) {
-            $convertedFrom = $this->convertFrom($from);
-            $message->from($convertedFrom['email'], $convertedFrom['name']);
-            $message->subject($db->subject);
-            $message->to($db->to, $db->toName);
-            if ($db->cc) {
-                $message->cc($this->toArray($db->cc));
-            }
-            if ($db->bcc) {
-                $message->bcc($this->toArray($db->bcc));
-            }
-        });
+        return Mail::send($template, $data, $this->getMaiable($this->findSlug($slug), $this->convertFrom($from)));
     }
 
     public function queue($data, $slug = null, $from = [], $template = 'emails.default')
     {
-        $db = $this->findSlug($slug);
-
         $data = (is_array($data)) ? $data : ['content' => $data];
-        return Mail::queue($template, $data, function ($message) use ($db, $from) {
-            $convertedFrom = $this->convertFrom($from);
-            $message->from($convertedFrom['email'], $convertedFrom['name']);
-            $message->subject($db->subject);
-            $message->to($db->to, $db->toName);
-            if ($db->cc) {
-                $message->cc($this->toArray($db->cc));
-            }
-            if ($db->bcc) {
-                $message->bcc($this->toArray($db->bcc));
-            }
-        });
+        return Mail::queue($template, $data, $this->getMaiable($this->findSlug($slug), $this->convertFrom($from)));
     }
 
     public function later($seconds, $data, $slug = null, $from = [], $template = 'emails.default')
     {
-        $db = $this->findSlug($slug);
-
         $data = (is_array($data)) ? $data : ['content' => $data];
-        return Mail::later($seconds, $template, $data, function ($message) use ($db, $from) {
-            $convertedFrom = $this->convertFrom($from);
-            $message->from($convertedFrom['email'], $convertedFrom['name']);
-            $message->subject($db->subject);
-            $message->to($db->to, $db->toName);
-            if ($db->cc) {
-                $message->cc($this->toArray($db->cc));
-            }
-            if ($db->bcc) {
-                $message->bcc($this->toArray($db->bcc));
-            }
-        });
+        return Mail::later($seconds, $template, $data, $this->getMaiable($this->findSlug($slug), $this->convertFrom($from)));
     }
 
-    private function findSlug($slug)
+    protected function findSlug($slug)
     {
         $query = MmailModel::active();
+
         if ($slug) {
             $query->where('slug', $slug);
         }
@@ -86,16 +43,30 @@ class Mmail
         return $db;
     }
 
-    private function toArray($emails)
+    protected function toArray($emails)
     {
         return explode(',', str_replace(' ', '', $emails));
     }
 
-    private function convertFrom($from)
+    protected function convertFrom($from)
     {
         $convertedFrom['email'] = isset($from[0]) ? $from[0] : 'noreply@mixd.com.br';
         $convertedFrom['name'] = isset($from[1]) ? $from[1] : 'MIXD Internet';
-
         return $convertedFrom;
+    }
+
+    protected function getMaiable($db, $from)
+    {
+        return function ($message) use ($db, $from) {
+            $message->from($from['email'], $from['name']);
+            $message->subject($db->subject);
+            $message->to($db->to, $db->toName);
+            if ($db->cc) {
+                $message->cc($this->toArray($db->cc));
+            }
+            if ($db->bcc) {
+                $message->bcc($this->toArray($db->bcc));
+            }
+        };
     }
 }
